a7c8c7e64eb288dad33b674dce3492509243edf1
Роман Лукьянчиков <64757074+AlmostDetermined@users.noreply.github.com>
2020-11-25 23:01:45 +0300

    Add files via upload

---
+++ task8.jl
@@ -0,0 +1,31 @@
+function moveWest!(r, n)
+    while n > 0
+        move!(r, West)
+        n -= 1
+    end
+end
+
+function moveOst!(r, n)
+    while n > 0
+        move!(r, Ost)
+        n -= 1
+    end
+end
+
+function main8!(r)
+    cnt = 0
+
+    while true
+        if isborder(r, Nord) == false
+            break
+        end
+
+        cnt += 1
+
+        if cnt % 2 == 1
+            moveWest!(r, cnt)
+        else
+            moveOst!(r, cnt)
+        end
+    end         
+end
