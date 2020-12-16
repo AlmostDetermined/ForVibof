1742fe3badcd6a7fb0f5a38521213660631d2166
Роман Лукьянчиков <64757074+AlwaysHasBeen@users.noreply.github.com>
2020-12-17 00:21:08 +0300

    NEW!!!

--- task1.jl
+++ task1.jl
@@ -1,21 +1,22 @@
-function mark_kross!(r::Robot) # - главная функция  
-    for side in (HorizonSide(i) for i=0:3) # - перебор всех возможных направлений
-        putmarkers!(r,side)
-        move_by_markers(r,inverse(side))
+inverse(origSide::HorizonSide) = HorizonSide(mod(Int(origSide) + 2, 4))
+function moveSide(r, side)
+    x = 0
+    while isborder(r, side) == false
+        move!(r, side)
+        putmarker!(r)
+        x += 1
     end
+
+    while x > 0
+        move!(r, inverse(side))
+        x -= 1
+    end
+end
+
+function main1!(r::Robot)
+    moveSide(r, Nord)
+    moveSide(r, Sud)
+    moveSide(r, Ost)
+    moveSide(r, West)
     putmarker!(r)
 end
-
-# Всюду в заданном направлении ставит маркеры вплоть до перегородки, но в исходной клетке маркер не ставит
-putmarkers!(r::Robot,side::HorizonSide) = while isborder(r,side)==false 
-    move!(r,side)
-    putmarker!(r)
-end
-
-# Перемещает робота в заданном направлении пока, пока он находится в клетке с маркером (в итоге робот окажется в клетке без маркера)
-move_by_markers(r::Robot,side::HorizonSide) = while ismarker(r)==true 
-    move!(r,side) 
-end
-
-# Возвращает направление, противоположное заданному
-inverse(side::HorizonSide) = HorizonSide(mod(Int(side)+2, 4))
